f09540e601086727f543ac18fc4a9884a1235383
Bryce Kilker <brycekilker@gmail.com>
2018-12-04 17:09:41 -0700

    progress by EOD Tuesday

--- server/index.js
+++ server/index.js
@@ -79,76 +79,79 @@ router.post("/labelimage", upload.any(), async (req, res) => {
 //     clientSecret: '4355750f8eff498bbb189804984be87f',
 //     redirectUri: 'http://localhost:8888'
 // })
-var scopes = ['user-read-private', 'user-read-email'],
-    redirectUri = 'http://localhost:8888/callback',
-    clientId = '8c5b0b3a7a2946c99a1f263a9fe7afbe',
-    state = 'some-state-of-my-choice';
+// var scopes = ['user-read-private', 'user-read-email'],
+//     redirectUri = 'http://localhost:8888/callback',
+//     clientId = '8c5b0b3a7a2946c99a1f263a9fe7afbe',
+//     state = 'some-state-of-my-choice';
 
-// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-var spotifyApi = new SpotifyWebApi({
-    redirectUri: redirectUri,
-    clientId: clientId
-});
+// // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+// var spotifyApi = new SpotifyWebApi({
+//     redirectUri: redirectUri,
+//     clientId: clientId
+// });
 
 // Create the authorization URL
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
 
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-console.log(authorizeURL);
+// console.log(authorizeURL);
+
+router.get("/callback", (req, res) => {
+    console.log("hello")
+
+    res.redirect('/')
+})
 
 var credentials = {
     clientId: '8c5b0b3a7a2946c99a1f263a9fe7afbe',
     clientSecret: '4355750f8eff498bbb189804984be87f',
-    redirectUri: 'http://localhost:8888/callback'
+    redirectUri: 'http://localhost:8888/callback',
+    scopes: ['user-read-private', 'user-read-email']
 };
 
 var spotifyApi = new SpotifyWebApi(credentials);
 
-// The code that's returned as a query parameter to the redirect URI
-var code = 'MQCbtKe23z7YzzS44KzZzZgjQa621hgSzHN';
-
-// Retrieve an access token and a refresh token
-spotifyApi.authorizationCodeGrant(code).then(
+spotifyApi.clientCredentialsGrant().then(
     function (data) {
-        console.log('The token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-        console.log('The refresh token is ' + data.body['refresh_token']);
-
-        // Set the access token on the API object to use it in later calls
-        spotifyApi.setAccessToken(data.body['access_token']);
-        spotifyApi.setRefreshToken(data.body['refresh_token']);
-    },
-    function (err) {
-        console.log('Something went wrong!', err);
-    }
-);
-// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
-spotifyApi.refreshAccessToken().then(
-    function (data) {
-        console.log('The access token has been refreshed!');
-
+        console.log(data)
         // Save the access token so that it's used in future calls
         spotifyApi.setAccessToken(data.body['access_token']);
     },
     function (err) {
-        console.log('Could not refresh access token', err);
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message
+        );
     }
 );
 
-// Set all credentials at the same time
-spotifyApi.setCredentials({
-    accessToken: 'myAccessToken',
-    refreshToken: 'myRefreshToken',
-    redirectUri: 'http://www.example.com/test-callback',
-    clientId: 'myClientId',
-    clientSecret: 'myClientSecret'
-});
 
-// Get all credentials
-console.log('The credentials are ' + spotifyApi.getCredentials());
+// Retrieve an access token and a refresh token
+// spotifyApi.authorizationCodeGrant(code).then(
+//     function (data) {
+//         console.log('The token expires in ' + data.body['expires_in']);
+//         console.log('The access token is ' + data.body['access_token']);
+//         console.log('The refresh token is ' + data.body['refresh_token']);
 
-// Reset all credentials at the same time
-spotifyApi.resetCredentials();
+//         // Set the access token on the API object to use it in later calls
+//         spotifyApi.setAccessToken(data.body['access_token']);
+//         spotifyApi.setRefreshToken(data.body['refresh_token']);
+//     },
+//     function (err) {
+//         console.log('Something went wrong!', err);
+//     }
+// );
+// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+// spotifyApi.refreshAccessToken().then(
+//     function (data) {
+//         console.log('The access token has been refreshed!');
+
+//         // Save the access token so that it's used in future calls
+//         spotifyApi.setAccessToken(data.body['access_token']);
+//     },
+//     function (err) {
+//         console.log('Could not refresh access token', err);
+//     }
+// );
 app.use(cors())
     .use(cookieParser());
 
@@ -159,6 +162,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+
+
+
 
 app.use(express.static(path.join(__dirname, './server/uploads')))
 
